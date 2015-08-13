efce8d1119c6db8f18296ae3efd2d1b5d41ac1d9
Prabhjot Singh Chandhar <prabhjotchandhar@gmail.com>
2015-08-14 01:44:04 +0530

    Factorial using Recursion

--- Chapter-2-Recursion/Factorial.java
+++ Chapter-2-Recursion/Factorial.java
@@ -11,6 +11,6 @@ public class Factorial {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(new Factorial().factorial(9));
+		System.out.println(new Factorial().factorial(Integer.parseInt(args[0])));
 	}
 }
