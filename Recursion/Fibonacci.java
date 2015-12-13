b8b1aca4b04eb0ba0af714f6a00ae60f9e24ce16
Prabhjot Singh Chandhar <prabhjotchandhar@gmail.com>
2015-12-13 15:56:00 +0530

    test commit

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -1,26 +1,25 @@
 package recursion;
 
 public class Fibonacci {
-	
+
 	static int first;
-	static int second=1;
-			static int sum = 0;
-	public void fib(int n){
-		
-		if(n==0){
-			//System.out.println("0");
-		}
-		else
-		{
-			
-			sum = first+second;
-			first=second;
-			second=sum;
-			System.out.print(sum+" ");
-			fib(n-1);
+	static int second = 1;
+	static int sum = 0;
+
+	public void fib(int n) {
+
+		if (n == 0) {
+			// System.out.println("0");
+		} else {
+
+			sum = first + second;
+			first = second;
+			second = sum;
+			System.out.print(sum + " ");
+			fib(n - 1);
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.print("0 1 ");
 		new Fibonacci().fib(23);
